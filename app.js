b7e6c98701b737ea900aeb319d83c675c1e9868a
smileyqp <468417415@qq.com>
2019-04-25 17:00:10 +0800

    polish websocket part

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = ws.createServer(function(conn){
     console.log('connected');
     conn.on('text',function(str){
         console.log(str);
-        conn.sendText(str);//将接收道德str用sendText方法传给接收到的一个连接
+        //conn.sendText(str);//将接收道德str用sendText方法传给接收到的一个连接
         boardcast(str);//调用广播方法将节后到的传给所有的浏览器
     });
     // setTimeout(function(){
